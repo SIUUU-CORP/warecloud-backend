b99550201e59ba4d896c412199505d608952e4aa
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2023-12-02 20:21:48 +0700

    feat: authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,35 @@ import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PrismaModule } from './prisma/prisma.module'
+import { AuthModule } from './auth/auth.module'
+import { CommonModule } from './common/common.module'
+import { APP_FILTER, APP_GUARD } from '@nestjs/core'
+import { HttpExceptionFilter } from './common/filters/http-exception.filter'
+import { AuthGuard } from './auth/auth.guard'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    CommonModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRY as string },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,91 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common'
+import { LoginDTO } from './DTO/login.DTO'
+import { PrismaService } from 'src/prisma/prisma.service'
+import * as bcrypt from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import { RegisterDTO } from './dto/register.dto'
+import { Role } from '@prisma/client'
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getUser(email: string) {
+    return await this.prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    })
+  }
+
+  async hashPassword(plainText: string): Promise<string> {
+    const salt = await bcrypt.genSalt()
+    const hashPassword = await bcrypt.hash(plainText, salt)
+    return hashPassword
+  }
+
+  async comparePassword(
+    password: string,
+    hashedPassword: string
+  ): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword)
+  }
+
+  async login(body: LoginDTO) {
+    const user = await this.getUser(body.email)
+
+    if (!user) {
+      throw new NotFoundException(`User with email ${body.email} is not found`)
+    }
+
+    const isPasswordValid = await this.comparePassword(
+      body.password,
+      user.password
+    )
+
+    if (!isPasswordValid) {
+      throw new BadRequestException('Invalid password')
+    }
+
+    const expiresIn = process.env.JWT_EXPIRY as string
+    const token = sign(
+      { id: user.id, email: user.email, role: user.role },
+      process.env.JWT_SECRET as string,
+      { expiresIn }
+    )
+
+    return { user, token }
+  }
+
+  async register(body: RegisterDTO) {
+    const { email, name, password, role, phoneNumber, address } = body
+
+    const hashedPassword = await this.hashPassword(password)
+
+    const user = await this.getUser(email)
+
+    if (!!user) {
+      throw new BadRequestException('Email is used')
+    }
+
+    const capitalizeRole = role.toUpperCase()
+    if (capitalizeRole !== 'CUSTOMER' && capitalizeRole !== 'VENDOR') {
+      throw new BadRequestException('Invalid role')
+    }
+
+    await this.prisma.user.create({
+      data: {
+        email: email,
+        name: name,
+        password: hashedPassword,
+        role: capitalizeRole === 'CUSTOMER' ? Role.CUSTOMER : Role.VENDOR,
+        phoneNumber: phoneNumber,
+        address: address,
+      },
+    })
+  }
+}
